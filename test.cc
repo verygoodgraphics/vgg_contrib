6e9f721b0a19acd2985736076821301565e4f5a7
Guanhua Hou <gaimanjing@gmail.com>
2022-12-13 09:22:48 +0800

    added: use wasmtime in test.cc

--- test.cc
+++ test.cc
@@ -1,16 +1,20 @@
+#include "MiniFB_cpp.h"
+#include "jpeglib.h"
 #include "nlohmann/json.hpp"
 #include "picosha2.h"
+#include "png.h"
+#include "quickjs/quickjs.h"
 #include "sdefl.h"
 #include "sinfl.h"
+#include "wasmtime.hh"
+#include "webp/decode.h"
+#include "webp/encode.h"
 #include "zlib.h"
 #include "zstd.h"
-#include "MiniFB_cpp.h"
-#include "quickjs/quickjs.h"
-#include "png.h"
-#include "jpeglib.h"
-#include "webp/encode.h"
-#include "webp/decode.h"
 
 int main() {
+
+  wasmtime::Engine engine;
+
   return 0;
 }
