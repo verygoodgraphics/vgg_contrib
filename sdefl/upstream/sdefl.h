80708c2402459c3e1faec953d8bc9573a3c85d8b
elf <elff1@outlook.com>
2024-01-17 18:30:32 +0800

    support build runtime on windows

--- sdefl/upstream/sdefl.h
+++ sdefl/upstream/sdefl.h
@@ -179,6 +179,9 @@ extern int zsdeflate(struct sdefl *s, void *o, const void *i, int n, int lvl);
 #include <assert.h> /* assert */
 #include <string.h> /* memcpy */
 #include <limits.h> /* CHAR_BIT */
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
 
 #define SDEFL_NIL               (-1)
 #define SDEFL_MAX_MATCH         258
